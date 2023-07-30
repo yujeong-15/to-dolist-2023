2103302df4f48e11ac913fb688397400e9d6211c
yujeong-15 <66857272+yujeong-15@users.noreply.github.com>
2023-07-30 22:33:13 +0900

    회원가입 기능80%
    todo:css, 에러메세지 할당 방법 생각

--- singup/singup.js
+++ singup/singup.js
@@ -1,47 +1,199 @@
+//아이디 변수 선언
 const userId = document.getElementById("userId");
-const passsword = document.getElementById("password");
+const errorId = document.querySelector(".error-id");
+
+//비밀번호 변수 선언
+const password = document.getElementById("password");
+const errorPassword = document.querySelector(".error-password");
+const eyes = document.querySelector(".fa-eye-slash");
+
+//이메일 변수 선언
+const email = document.getElementById("email");
+const errorEmail = document.querySelector(".error-email");
+
+//이름 변수 선언
 const userName = document.getElementById("userName");
-const isValid = document.querySelector(".userIdBox .isValid");
-console.log(isValid);
+const errorUserName = document.querySelector(".error-userName");
 
 
-userId.addEventListener('change', onChangeInput);
-//todo:전 유효성 체크 잡는 방법, input실시간 감지 이벤트 찾아보기
-function onChangeInput(){
-    isValid.style.visibility = 'visible';
-    if(isRequiredFieldChecked()){
+//생년월일 변수 선언
+const date = document.getElementById("date");
+const errorDate = document.querySelector(".error-date");
 
+const form = document.querySelector("#singup-from");
+form.addEventListener("submit", onSubmit);
+
+
+//전체 input
+const input = document.getElementsByClassName("text");
+const inputArray = [...input];
+
+//아이디 기능
+userId.addEventListener("blur", () => {
+    if (userId.value === "") {
+        userId.classList.add("error-input");
+        errorId.innerHTML = "필수 정보입니다."
+        errorId.style.visibility = "visible";
     }
-    isRequiredFieldChecked();
-    spaceCheck();
-    isValidId();
+});
+
+function handleIdChange() {
+    const pattern = /^(?![0-9])[a-zA-Z](?!.*[^a-zA-Z0-9]).{3,19}$/;
+    errorId.style.visibility = "visible";
+    if (userId.value === "") {
+        errorId.innerHTML = "필수 정보입니다.";
+        userId.classList.add("error-input");
+    } else if (!pattern.test(userId.value)) {
+        errorId.innerHTML = "4~20자의 영대소문자, 숫자만 사용 가능합니다. 다만, 숫자로 시작할 수 없습니다.";
+        userId.classList.add("error-input");
+    } else {
+        userId.classList.remove("error-input");
+        errorId.style.visibility = "hidden";
+        errorId.innerHTML = "";
+    }
 }
 
-function spaceCheck() {
-    const spaceRegex = /^\s+|\s+$|\s+(?=\s)/g;
-    console.log(userId.value);
-    if (spaceRegex.test(userId.value)) {
-        console.log("Ddd");
-        isValid.innerText = "공백이 들어가 있습니다.";
 
-        return true;
+//패스워드 기능
+password.addEventListener("blur", () => {
+    if (password.value === "") {
+        password.classList.add("error-input");
+        errorPassword.innerHTML = "필수 정보입니다."
+        errorPassword.style.visibility = "visible";
+    }
+});
+
+function handlePasswordChange() {
+    const pattern = /^(?!((?:[A-Za-z]+)|(?:[~!@#$%^&*()_+=]+)|(?:[0-9]+))$)[A-Za-z\d~!@#$%^&*()_+=]{8,16}$/;
+    errorPassword.style.visibility = "visible";
+    if (password.value === "") {
+        errorPassword.innerHTML = "필수 정보입니다.";
+        password.classList.add("error-input");
+
+    } else if (!pattern.test(password.value)) {
+        errorPassword.innerHTML = "8~16자의 영대소문자, 숫자, 특수문자 중  2종류 문자조합으로 해주세요";
+        password.classList.add("error-input");
+
+    } else {
+        password.classList.remove("error-input");
+        errorPassword.style.visibility = "hidden";
+        errorPassword.innerHTML = "";
     }
 }
 
-function isValidId() {
-    const regex = /^[a-z]+[a-z0-9]{5,19}$/g;
-    if (!regex.test(userId.value)) {
-         isValid.innerText = "6~20글자 이내의 영문자와 숫자로 이루어진 아이디를 입력해주세요";
-        return true;
+eyes.addEventListener("click", () => {
+    eyes.classList.toggle("fa-eye");
+    eyes.classList.toggle("fa-eye-slash");
+    if (eyes.classList.contains("fa-eye-slash")) {
+        password.setAttribute("type", "password");
+    } else {
+        password.setAttribute("type", "text");
     }
-    
-}
-function isRequiredFieldChecked() {
-    const pattern = /^.+$/;
-    if (!pattern.test(userId.value)) {
-         isValid.innerText = "필수 항목입니다";
-         return true;
+});
+
+
+//이메일 기능
+function handleEmailChange() {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailPattern.test(email.value)) {
+        errorEmail.style.visibility = "visible";
+        email.classList.add("error-input");
+        errorEmail.innerHTML = "이메일 주소가 정확한지 확인해 주세요.";
+    } else {
+        email.classList.remove("error-input");
+        errorPassword.style.visibility = "hidden";
+        errorEmail.innerHTML = "";
     }
-    return false;
 }
 
+
+//이름 기능
+userName.addEventListener("blur", () => {
+    if (userName.value === "") {
+        userName.classList.add("error-input");
+        errorUserName.innerHTML = "필수 정보입니다."
+        errorUserName.style.visibility = "visible";
+    }
+});
+
+
+function handleNameChange() {
+    const pattern = /^[가-힣a-zA-Z]+$/;
+
+    if (userName.value === "") {
+        errorUserName.innerHTML = "필수 정보입니다.";
+        userName.classList.add("error-input");
+        errorUserName.style.visibility = "visible";
+    } else if (!pattern.test(userName.value)) {
+        errorUserName.innerHTML = "한글, 영문 대/소문자를 사용해 주세요. (특수기호, 공백 사용 불가)";
+        userName.classList.add("error-input");
+        errorUserName.style.visibility = "visible";
+    } else {
+        userName.classList.remove("error-input");
+        errorUserName.style.visibility = "hidden";
+        errorUserName.innerHTML = "";
+    }
+}
+
+
+//생년월일 기능
+date.addEventListener("blur", () => {
+    if (date.value === "") {
+        date.classList.add("error-input");
+        errorDate.innerHTML = "필수 정보입니다."
+        errorDate.style.visibility = "visible";
+    }
+});
+
+function handleDateChange() {
+    const pattern = /^[0-9]*$/;
+    if (date.value === "") {
+        date.classList.add("error-input");
+        errorDate.innerHTML = "필수 정보입니다."
+        errorDate.style.visibility = "visible";
+    } else if (!pattern.test(date.value) && 1 < date.value.length && date.value.length < 8) {
+        console.log("xxx");
+    }
+}
+
+function handleDateChange() {
+    const numberPattern = /^\d{8}$/;
+    const pattern = /^(191[4-9]|19[2-9]\d|200\d|201[0-9]|202[0-3])(0[1-9]|1[012])(0[1-9]|[12][0-9]|3[01])$/;
+
+
+    if (date.value === "") {
+        date.classList.add("error-input");
+        errorDate.innerHTML = "필수 정보입니다.";
+        errorDate.style.visibility = "visible";
+    } else if (!numberPattern.test(date.value)) {
+        date.classList.add("error-input");
+        errorDate.style.visibility = "visible";
+        errorDate.innerHTML = "생년월일은 8자리 숫자로 입력해 주세요.";
+    } else {
+        if (!pattern.test(date.value)) {
+            date.classList.add("error-input");
+            errorDate.style.visibility = "visible";
+            errorDate.innerHTML = "생년월일이 정확한지 확인해 주세요.";
+        } else {
+            date.value = date.value.replace(/^(\d{4})(\d{2})(\d{2})$/, "$1-$2-$3");
+            date.classList.remove("error-input");
+            errorDate.style.visibility = "hidden";
+            errorDate.innerHTML = "";
+        }
+        date.value = date.value.replace(/^(\d{4})(\d{2})(\d{2})$/, "$1-$2-$3");
+    }
+}
+
+//todo 각에러에 맞는 메세지 할당,css 반응형
+function onSubmit(event) {
+    inputArray.forEach((text) => {
+        if (text.classList.contains("error-input")) {
+            event.preventDefault();
+        }else if(text.value === ""){
+            text.classList.add("error-input");
+            text.innerHTML = "필수 정보입니다.";
+            text.style.visibility = "visible";
+        }
+    });
+   
+}
